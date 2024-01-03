9f3c209fe7bbd682575e08ae96301327d6c1b884
Haya S A M A Alqahtani <83316@kfh.com>
2024-01-03 13:43:34 +0300

    loops

---
+++ WeekdaysPrinter.java
@@ -0,0 +1,20 @@
+public class WeekdaysPrinter {
+    public static void main(String[] args) throws Exception {
+        String[] Daysoftheweek ={"sunday" , "monday" , "tuesday" , "wednesday","thursday"
+        , "friday", "saturday"};
+
+        for(int i =0; i< Daysoftheweek.length; i++){
+         if (i>=5)break; 
+             System.out.println(Daysoftheweek[i]);
+
+         }
+        
+    }
+
+    
+
+
+    }
+
+
+
